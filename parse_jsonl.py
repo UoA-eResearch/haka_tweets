6d02d5083d2e2154419fb7c73f7ed828a08890d1
Nick <nick.young@auckland.ac.nz>
2020-01-15 17:00:35 +1300

    init

---
+++ parse_jsonl.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+df = pd.read_json("tweets.jsonl", lines=True)
+df["screen_name"] = df.user.apply(lambda x: x.get("screen_name"))
+df["location"] = df.place.apply(lambda x: x["full_name"] + ", " + x["country"] if x is not None else None)
+df["bounding_box"] = df.place.apply(lambda x: x["bounding_box"] if x else None)
+df[["id", "created_at", "screen_name", "full_text", "favorite_count", "retweet_count", "location", "bounding_box"]].to_csv("lsu_quote_replies.csv", index=False)
