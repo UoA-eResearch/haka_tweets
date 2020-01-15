da437c966f9ab311b27ca63f2aacf8b930441ca4
Nick <nick.young@auckland.ac.nz>
2020-01-16 11:21:25 +1300

    add another 200 reply tweet ids

--- parse_jsonl.py
+++ parse_jsonl.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import sys
 
-df = pd.read_json("tweets.jsonl", lines=True)
+df = pd.read_json(sys.argv[1], lines=True)
 df["screen_name"] = df.user.apply(lambda x: x.get("screen_name"))
 df["location"] = df.place.apply(lambda x: x["full_name"] + ", " + x["country"] if x is not None else None)
 df["bounding_box"] = df.place.apply(lambda x: x["bounding_box"] if x else None)
