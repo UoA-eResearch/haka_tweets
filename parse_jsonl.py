49b91296a7b8c7d56d1ac56e46463a655199a3db
Nick <nick.young@auckland.ac.nz>
2020-01-29 17:12:01 +1300

    add some more tweet ids extracted from treeverse

--- parse_jsonl.py
+++ parse_jsonl.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-import sys
 
-df = pd.read_json(sys.argv[1], lines=True)
+df = pd.read_json("lsu_quote_replies.jsonl", lines=True)
 df["screen_name"] = df.user.apply(lambda x: x.get("screen_name"))
 df["location"] = df.place.apply(lambda x: x["full_name"] + ", " + x["country"] if x is not None else None)
 df["bounding_box"] = df.place.apply(lambda x: x["bounding_box"] if x else None)
-df[["id", "created_at", "screen_name", "full_text", "favorite_count", "retweet_count", "location", "bounding_box"]].to_csv("lsu_quote_replies.csv", index=False)
+df["in_reply_to_status_id"] = df["in_reply_to_status_id"].astype('Int64')
+df[["id", "created_at", "screen_name", "full_text", "favorite_count", "retweet_count", "in_reply_to_status_id", "location", "bounding_box"]].to_csv("lsu_quote_replies.csv", index=False)
